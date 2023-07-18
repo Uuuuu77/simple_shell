a22628a2ef5411a0d6b4b21151c97a3f43999008
Abokiey <sweneysam@gmail.com>
2023-07-18 13:06:24 +0300

    not_found.c

--- not_found.c
+++ not_found.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * prompt - Entry point-> Prints the prompt message
+ * prompt - function Prints the prompt message
 */
 void prompt(void)
 {
@@ -11,10 +11,10 @@ void prompt(void)
 }
 
 /**
- * cmd_not_found - Entry point-> It prints error statement when cmd not found.
- * @av: input arguments list
- * @counter: input commands counter
- * @cmd_list: input list of commands
+ * cmd_not_found - function prints error statement when cmd not found.
+ * @av: arguments list
+ * @counter: commands counter
+ * @cmd_list: the list of commands
  * @path: input path list
 */
 void cmd_not_found(char **av, int counter, char **cmd_list, char **path)
@@ -31,7 +31,7 @@ void cmd_not_found(char **av, int counter, char **cmd_list, char **path)
 }
 
 /**
- * getline_err - Entry point-> It sets new line & exits when it return -1.
+ * getline_err - function sets new line & exits when it return -1.
  * @user_input: input string entered by user
 */
 void getline_err(char *user_input)
