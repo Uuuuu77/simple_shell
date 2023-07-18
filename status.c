185c7dcefd89160ccea9e4e797b6a753bfeba9b2
Abokiey <sweneysam@gmail.com>
2023-07-18 13:17:09 +0300

    test path

--- status.c
+++ status.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 
 /**
- * test_path - Entry point-> It finds the path of a given command.
- * @paths: input array of strings of the paths
- * @cmd_list: input array of strings of command passed
- * Return: similar path (Success)
+ * test_path - function finds the path of an input command.
+ * @paths: an array of strings of the paths
+ * @cmd_list: an array of strings of command passed
+ * Return: similar path if Successful
 */
 char *test_path(char **paths, char **cmd_list)
 {
