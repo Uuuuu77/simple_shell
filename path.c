b91e2c1e7a337d4edfab837e932db458e2cbb95e
Abokiey <sweneysam@gmail.com>
2023-07-18 13:11:36 +0300

    path

--- path.c
+++ path.c
@@ -1,15 +1,15 @@
 #include "shell.h"
 
 /**
- * path_list - Entry point-> So it gets the PATH and splits it.
- * @path: input path to split
- * @cmd: input command to test it's path
- * Return: array of strings of paths (Success)
+ * path_list - function gets the PATH and splits it.
+ * @path: path to split
+ * @cmd: input tests it's path
+ * Return: array of strings of paths if successful
 */
 char **path_list(char *path, char *cmd)
 {
 	char **path_s = NULL;
-	char *token, *cp_path, *delim = ":";
+	char *tkn, *cp_path, *delim = ":";
 	int path_size = _strlen(path) + 1;
 	int path_letters = word_count(path) + 1;
 	int b = 0;
@@ -19,15 +19,15 @@ char **path_list(char *path, char *cmd)
 
 	path_s = malloc(sizeof(path_s) * path_letters);
 
-	token = strtok(cp_path, delim);
-	while (token != NULL)
+	tkn = strtok(cp_path, delim);
+	while (tkn != NULL)
 	{
-		path_s[b] = malloc(sizeof(token) * (_strlen(token) + 2));
-		_strcpy(path_s[b], token);
+		path_s[b] = malloc(sizeof(tkn) * (_strlen(tkn) + 2));
+		_strcpy(path_s[b], tkn);
 		_strcat(path_s[b], "/");
 		_strcat(path_s[b], cmd);
 		_strcat(path_s[b], "\0");
-		token = strtok(NULL, delim);
+		tkn = strtok(NULL, delim);
 		b++;
 	}
 	path_s[b] = NULL;
